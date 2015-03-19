f8fe1b62eb7fa4bc91d1c10a3f9fee8be79dc721
nucl <hirsch617@gmail.com>
2015-03-18 23:17:37 -0400

    raw write, then FLAC or MP3 or WAV

--- generator.py
+++ generator.py
@@ -14,7 +14,7 @@ For indefinitely long iteration over the samples, consider using the output of t
 Noise
 *****
 
-Different types of noise are available. The following table lists the color 
+Different types of noise are available. The following table lists the color
 of noise and how the power and power density change per octave.
 
 ====== ===== =============
@@ -42,8 +42,8 @@ For related functions, check :mod:`scipy.signal`.
 """
 
 import numpy as np
-import random
-import itertools
+#import random
+#import itertools
 #import scipy.signal.sawtooth
 
 try:
@@ -56,49 +56,46 @@ from signal_acoustics import normalise
 
 def noise(N, color='white'):
     """Noise generator.
-    
+
     :param N: Amount of samples.
     :param color: Color of noise.
-    
+
     """
     try:
         return noise_generators[color](N)
     except KeyError:
-        raise ValueError("Incorrect color.")
-
-
+        print('** unknown color ' + str(color) + ', falling back to pink noise.')
+        return noise_generators['pink'](N)
 
 def white(N):
     """
     White noise.
-    
+
     :param N: Amount of samples.
-    
+
     White noise has a constant power density. It's narrowband spectrum is therefore flat.
-    The power in white noise will increase by a factor of two for each octave band, 
+    The power in white noise will increase by a factor of two for each octave band,
     and therefore increases with 3 dB per octave.
     """
-    return np.random.randn(N)
-
-
+    return np.random.randn(N).astype(np.float32)
 
 def pink(N):
     """
-    Pink noise. 
-    
+    Pink noise.
+
     :param N: Amount of samples.
-    
+
     Pink noise has equal power in bands that are proportionally wide.
     Power density decreases with 3 dB per octave.
-    
+
     """
     # This method uses the filter with the following coefficients.
     #b = np.array([0.049922035, -0.095993537, 0.050612699, -0.004408786])
     #a = np.array([1, -2.494956002, 2.017265875, -0.522189400])
     #return lfilter(B, A, np.random.randn(N))
-    
+
     # Another way would be using the FFT
-    x = np.random.randn(N)
+    x = white(N)
     X = rfft(x) / N
     S = np.sqrt(np.arange(len(X))+1.) # +1 to avoid divide by zero
     y = (irfft(X/S)).real[0:N]
@@ -107,15 +104,15 @@ def pink(N):
 
 def blue(N):
     """
-    Blue noise. 
-    
+    Blue noise.
+
     :param N: Amount of samples.
-    
+
     Power increases with 6 dB per octave.
-    Power density increases with 3 dB per octave. 
-    
+    Power density increases with 3 dB per octave.
+
     """
-    x = np.random.randn(N)
+    x = white(N)
     X = rfft(x) / N
     S = np.sqrt(np.arange(len(X)))# Filter
     y = (irfft(X*S)).real[0:N]
@@ -125,14 +122,14 @@ def blue(N):
 def brown(N):
     """
     Violet noise.
-    
+
     :param N: Amount of samples.
-    
+
     Power decreases with -3 dB per octave.
-    Power density decreases with 6 dB per octave. 
+    Power density decreases with 6 dB per octave.
 
     """
-    x = np.random.randn(N)
+    x = white(N)
     X = rfft(x) / N
     S = (np.arange(len(X))+1)# Filter
     y = (irfft(X/S)).real[0:N]
@@ -141,15 +138,15 @@ def brown(N):
 
 def violet(N):
     """
-    Violet noise. Power increases with 6 dB per octave. 
-    
+    Violet noise. Power increases with 6 dB per octave.
+
     :param N: Amount of samples.
-    
+
     Power increases with +9 dB per octave.
-    Power density increases with +6 dB per octave. 
-    
+    Power density increases with +6 dB per octave.
+
     """
-    x = np.random.randn(N)
+    x = white(N)
     X = rfft(x) / N
     S = (np.arange(len(X)))# Filter
     y = (irfft(X*S)).real[0:N]
@@ -167,20 +164,20 @@ noise_generators = {
 
 def noise_frequency_dependent(N, frequencies, gains):
     """Frequency-dependent noise signal.
-    
+
     :param N: Amount of samples.
     :param frequencies: Normalized frequencies.
     :param gains: Gains.
     :param gains: Gain for given frequency.
-    :type gains: List of tuples or 2xN array. The first item in the 
+    :type gains: List of tuples or 2xN array. The first item in the
 
 
 
 #def sawtooth(f, fs, width=1):
     #"""
     #Sawtooth. Return f sawteeth per second.
-    
-    
+
+
     #"""
     #t = np.arange()
 
@@ -205,7 +202,7 @@ def noise_frequency_dependent(N, frequencies, gains):
 #def generate(iterable):
     #"""
     #Return indefinetely long generator of iterable.
-    
+
     #.. note:: :func:`itertools.cycle`
     #"""
     #return itertools.cycle(iterable)
@@ -216,12 +213,12 @@ def noise_frequency_dependent(N, frequencies, gains):
     #Sawtooth generator. Repeat....
     #"""
     #raise NotImplementedError
-    
+
 
 def heaviside(N):
     """Heaviside.
-    
+
     Returns the value 0 for `x < 0`, 1 for `x > 0`, and 1/2 for `x = 0`.
     """
     return 0.5 * (np.sign(N) + 1)
-    
+
--- soothing.py
+++ soothing.py
@@ -5,32 +5,65 @@ GPL v3
 based on python-acoustics noise generator code
 generates soothing sounds, e.g. for sleep enhancement
 
+SUGGESTED USE:
+write file to SD card, use it in speaker/phone capable of playing FLAC
+Example (assuming fs=16000):
+python2 soothing.py pink 8 -o test.raw   # 931MB file, 8 hours of pink noise
+ffmpeg -f s16le -ar 16000 -ac 1 -i test.raw test.flac #760.4MB file  (or just .wav, gives same size file as .raw)
+
+
 future: add GPIO input for Raspberry Pi, Beaglebone, Edison, etc.
+
+I demonstrate using a few different packages.
+The simplest way to use this program is by writing a file to disk without playing audio,
+you can write a WAV file with
+scipy (widely available)
+or
+scikits.audiolab (a little trickier to install)
+
+saving as RAW requires using an external program like FFMPEG or Goldwave to convert
+to a more common format. The huge advantage of RAW is that you can iteratively
+write several hours of random noise without consuming all your RAM.
+
 """
 from __future__ import division
 import sys
 import importlib
 import numpy as np
+import os
 #
 from generator import noise
 from time import sleep, time
 
-soundmod = 'pygame'#'pyglet'#'pyaudio' #'pygame'
-smod = importlib.import_module(soundmod)
+soundmod = 'pygame'#'pyglet'#'pyaudio' #'pygame' #'scikits.audiolab'
+wavapi = 'raw' #'skaudio' #'scipy'
 
-fs = 44100 #[Hz] sample rate of sound card playback
+fs = 16000 #[Hz] sample rate of sound card playback
 nsec = 60 #unique sound length -- 600 sec was too much for 512MB Rpi in certain noise modes that do advanced computations
+nbitfile = 16
+nbitfloat = 32 #from generator.py
 
-def main(ntype,nhours):
-    ramused = fs*nsec*64//8 #bytes
+def computenoise(ntype):
+    nsamp = fs*nsec
+    ramused = nsamp*nbitfloat//8 #bytes, assuming np.float64, does NOT account for copies!
     if ramused>128e6:
-        exit('*** using more than 128MB of RAM for samples, this can be too much for Raspberry Pi, exiting')
+        print('*** using more than 128MB of RAM for samples, this can be too much for Raspberry Pi')
+
+    rawused = ramused//(nbitfloat//nbitfile)
+    if rawused>1e9:
+        print('** caution, your raw output is {:0.1f}'.format(rawused/1e9) + ' GB of data.')
+
     print('sound samples used at least {:0d}'.format(ramused) + ' bytes of RAM to create')
     ntype = ntype.lower()
     tic = time()
-    samps = (noise(nsec*fs, color=ntype) * 32768/8).astype(np.int16) #TODO arbitary scaling to 16-bit, noise() outputs float64
-    print('it took {:0.1f}'.format(time()-tic) + ' seconds to compute {:0.0f}'.format(nsec) + ' sec. of ' + ntype + ' noise.')
+    samps = (noise(nsamp, color=ntype) * 32768/8).astype(np.int16) #TODO arbitary scaling to 16-bit, noise() outputs float64
+    print('it took {:0.1f}'.format(time()-tic) + ' seconds to compute {:0.0f}'.format(nsec) +
+          ' sec. of ' + ntype + ' noise.')
     print('max sample value {:0.0f}'.format(samps.max()))
+    return samps
+
+def liveplay(samps,nhours):
+    smod = importlib.import_module(soundmod)
 
     if soundmod == 'pyaudio':
         p = smod.PyAudio()
@@ -47,6 +80,8 @@ def main(ntype,nhours):
         print('pygame volume level: ' + str(sound.get_volume()))
         print('sound playing for {:0.2f}'.format(sleepsec/3600) + ' hours')
         sleep(sleepsec) #seconds
+    elif soundmod=='scikits.audiolab':
+        smod.play(samps)
     elif soundmod=='pyglet':
         print('pyglet not yet implemented')
 #        """
@@ -60,11 +95,37 @@ def main(ntype,nhours):
         print('unknown sound module' + str(soundmod))
         sys.exit(1)
 
+def savenoise(samps,nhours,ofn):
+    if ofn is not None:
+        if wavapi == 'raw':
+            try: #delete because we're going to append
+                os.remove(ofn)
+            except OSError:
+                pass
+
+            with open(ofn,'a+b') as f:
+                for i in range(int(nhours*3600/nsec)):
+                    f.write(samps)
+        elif wavapi == 'scipy':
+            from scipy.io import wavfile
+            wavfile.write(ofn,fs,samps)
+        elif wavapi=='skaudio':
+            from scikits.audiolab import Format,Sndfile
+            fmt = Format('flac')
+            f = Sndfile(ofn,'w',fmt, 1, 16000) #too old to work with "with...." syntax
+            f.write_frames(samps)
+            f.close()
+
 if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description="noise generation program for Raspberry Pi or any Python-capable computer")
     p.add_argument('nmode',help='what type of white noise [white, pink, brown...]',type=str,nargs='?',default='pink')
     p.add_argument('hours',help='how many hours do you want sound generated for [default=8 hours]',type=float,nargs='?',default=8)
+    p.add_argument('-o','--ofn',help='output .wav filename',type=str,default=None)
     a = p.parse_args()
 
-    main(a.nmode, a.hours)
+    samps = computenoise(a.nmode)
+    if a.ofn is None:
+        liveplay(samps, a.hours)
+    else:
+        savenoise(samps,a.hours,a.ofn)
