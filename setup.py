cca87d83f9246f0dcd40ea7d29bc3073c97f5b7c
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-05-24 15:21:17 -0400

    template

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
-from setuptools import setup
+req = ['nose','numpy','scipy']
 
-req = ['numpy','scipy','pyfftw']
+import pip
+try:
+    import conda.cli
+    conda.cli.main('install',*req)
+except ImportError:    
+    pip.main(['install'] + req)
+# %%
+from setuptools import setup
 
 setup(name='soothingsounds',
       packages=['soothingsounds'],
       author='Michael Hirsch, Ph.D',
       url = 'https://github.com/scivision/soothing-sounds',
       install_requires=req,
+      extras_require={'pyfftw':['pyfftw']},
 	  )
